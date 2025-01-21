6459184119d3bf26b6c407c0cea6ef25e258f90a
ramedina98 <rmedinamartindelcampo@gmail.com>
2025-01-20 18:44:22 -0600

    merge: electron to main

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,6 @@
 import { defineConfig } from 'vite'
-import path from 'node:path'
-import electron from 'vite-plugin-electron/simple'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',
-  plugins: [
-    react(),
-    electron({
-      main: {
-        entry: 'electron/main.ts',
-      },
-      preload: {
-        input: path.join(__dirname, 'electron/preload.ts'),
-      },
-      renderer: {},
-    }),
-  ],
+  plugins: [react()],
 })
